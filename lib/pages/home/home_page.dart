ebbefb80d879a1d67fa5e56f71ca3ba5236a4918
gaoguanqi <307590625@qq.com>
2019-05-07 17:00:01 +0800

    home

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -338,7 +338,7 @@ class _ContentItem extends StatelessWidget {
                       Column(
                         children: <Widget>[
                           Text(_list[_index].time),
-                          Icon(Icons.access_time)
+                          Icon(Icons.access_time,size: 12.0,)
                         ],
                       ),
                     ],
